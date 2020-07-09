4f213ffcb9cb28accc2ec032f3883766206298b3
Vansh <vansharora2929@gmail.com>
2020-07-09 23:15:00 +0530

    Script complete

--- makingString.py
+++ makingString.py
@@ -1,2 +1,13 @@
 # Pass a department name and a list of users belonging to that department.
 # Return a string as dept: names
+
+def group_list(group, users):
+    members = group + ":" +" "
+    for user in users:
+        members+= user+','
+    members = members[:len(members)-1]
+    return members
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
